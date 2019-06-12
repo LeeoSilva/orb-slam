043bee27caad1b6fe71ecf5477e0c9e43e3324fa
LeeoSilva <LeeoSilva0909@gmail.com>
2019-06-11 23:02:11 -0300

    now it works

--- headers/featureMatcher.hpp
+++ headers/featureMatcher.hpp
@@ -1,18 +1,26 @@
 #pragma once
 #include <vector>
+#include <utility>
 #include <opencv2/opencv.hpp>
 
 class featureMatcher{
-private:
-	std::vector<cv::DMatch> matches;
-
-public:
-	void match(const cv::Mat& descriptors1, const cv::Mat& descriptors2);
 	cv::Mat paint(const cv::Mat& actframe, 
-			const std::vector<cv::KeyPoint>& keypoints1, 
-			const cv::Mat& prevFrame,
-			const std::vector<cv::KeyPoint>& keypoints2);
+		const cv::Mat& prevFrame,
+		const std::vector<cv::KeyPoint>& keypoints1, 
+		const std::vector<cv::KeyPoint>& keypoints2,
+		const std::vector<cv::DMatch>& matches);
+
+
 	void filter(const unsigned& maxDistance, const unsigned& minDistance);
 	void sortByDistance();
-	std::vector<cv::DMatch> getTopMatches(const unsigned& top);
+
+	//std::vector<std::vector<cv::DMatch>> match(const cv::Mat& descriptors1, const cv::Mat& descriptors2);
+	std::vector<cv::DMatch> match(const cv::Mat& descriptors1, const cv::Mat& descriptors2);
+	std::vector<cv::DMatch> knnMatch(const cv::Mat& descriptors1, const cv::Mat& descriptors2, const unsigned& k);
+
+	cv::Mat paint(const cv::Mat& actFrame, 
+					const std::vector<cv::KeyPoint>& keypoints1, 
+					const cv::Mat& prevFrame,
+					const std::vector<cv::KeyPoint>& keypoints2,
+					const std::vector<cv::DMatch>& matches);
 };
--- src/featureMatcher.cpp
+++ src/featureMatcher.cpp
@@ -1,41 +1,74 @@
 #include "../headers/featureMatcher.hpp"
-
 // For explanation of how does Feature Matching works go to:
 // https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_feature2d/py_matcher/py_matcher.html
-
-void featureMatcher::match(const cv::Mat& descriptors1, const cv::Mat& descriptors2){
+/*
+std::vector<std::vector<cv::DMatch>> featureMatcher::match(const cv::Mat& descriptors1, const cv::Mat& descriptors2){
+	std::vector<std::vector<cv::DMatch>> matches;
 	cv::BFMatcher matcher(cv::NORM_HAMMING, true); // Brute force matcher 
-	matcher.match(descriptors1, descriptors2, this->matches);
-	std::cout << "\rGot " << this->matches.size() << " matches" << std::endl;
+	matcher.match(descriptors1, descriptors2, matches);
+	std::cout << "\rGot " << (matches.size() * matches[0].size())  << " matches" << std::endl;
+	return matches;
 }
+*/
+std::vector<cv::DMatch> featureMatcher::match(const cv::Mat& descriptors1, const cv::Mat& descriptors2){
+	std::vector<cv::DMatch> matches;
+	cv::BFMatcher matcher(cv::NORM_HAMMING, true); // Brute force matcher 
+	matcher.match(descriptors1, descriptors2, matches);
+	std::cout << "Got " << matches.size()  << " matches" << std::endl;
+	return matches;
+}
+
+std::vector<std::vector<cv::DMatch>> featureMatcher::knnMatch(const cv::Mat& descriptors1, const cv::Mat& descriptors2, const unsigned& k){
+	std::vector<std::vector<cv::DMatch>> matches;
+	cv::BFMatcher matcher(cv::NORM_HAMMING, true); // Brute force matcher 
+    matcher.knnMatch(descriptors1, descriptors2, matches, k);
+	return matches;
+}
+
 
 cv::Mat featureMatcher::paint(const cv::Mat& actFrame, 
 				const std::vector<cv::KeyPoint>& keypoints1, 
 				const cv::Mat& prevFrame,
-				const std::vector<cv::KeyPoint>& keypoints2){
+				const std::vector<cv::KeyPoint>& keypoints2,
+				const std::vector<cv::DMatch>& matches)
+{
 	cv::Mat imgMatches; // output image
-	drawMatches(actFrame, keypoints1, prevFrame, keypoints2, this->matches, imgMatches);
+	drawMatches(actFrame, keypoints1, prevFrame, keypoints2, matches, imgMatches);
 	return imgMatches;
 }
+/*
+std::vector<std::vector<cv::DMatch>> featureMatcher::filter(const unsigned& maxDistance, const unsigned& minDistance, const std::vector<std::vector<cv::DMatch>> matches){
+	std::vector<std::vector<cv::DMatch>> filteredMatches;
+	for(std::size_t i = 0; i <= matches.size(); ++i)
+		for(std::size_t j = 0; j <= matches[i].size; ++j){
+			 bool cond = (matches[i].distance > minDistance && matches[i].distance < maxDistance);
+			if(cond) filteredMatches.emplace_back(matches[i]);
+		}
 
-void featureMatcher::filter(const unsigned& maxDistance, const unsigned& minDistance){
-	std::vector<cv::DMatch> filteredMatches; 
-	for(std::size_t i = 0; i <= this->matches.size(); ++i){
-		if(matches[i].distance > minDistance) filteredMatches.push_back(matches[i]);
-		else if(matches[i].distance < maxDistance) filteredMatches.push_back(matches[i]);
-	}
-	this->matches = filteredMatches;
+	return filteredMatches;
 }
 
-void featureMatcher::sortByDistance(){
+
+std::vector<cv::DMatch> featureMatcher::filter(const unsigned& maxDistance, const unsigned& minDistance, const std::vector<cv::DMatch> matches){
+	std::vector<cv::DMatch> filteredMatches;
+	for(std::size_t i = 0; i <= matches.size(); ++i)
+		for(std::size_t j = 0; j <= matches[i].size; ++j){
+			 bool cond = (matches[i].distance > minDistance && matches[i].distance < maxDistance);
+			if(cond) filteredMatches.emplace_back(matches[i]);
+		}
+
+	return filteredMatches;
+}
+
+
+
+void featureMatcher::sortByDistance(const std::vector<cv::DMatch>& matches){
 	std::vector<cv::DMatch> sortedMatches;
-	for(std::size_t i = 0; i <= this->matches.size(); ++i){
-		std::cout << "Distance: " << this->matches[i].queryIdx<< std::endl;
-	}
+	for(std::size_t i = 0; i <= matches.size(); ++i)
+		std::cout << "Distance: " << matches[i].distance << std::endl;
 }
-
-
+ 
 std::vector<cv::DMatch> featureMatcher::getTopMatches(const unsigned& top){
 
 }
-
+*/
