7fd1720386a61188392812aabe8498c1d75c114c
LeeoSilva <LeeoSilva0909@gmail.com>
2019-06-14 00:14:51 -0300

    backup

--- headers/featureExtractor.hpp
+++ headers/featureExtractor.hpp
@@ -1,19 +1,23 @@
 #pragma once
-#include <vector>
 #include <opencv2/opencv.hpp>
 #include <utility>
+#include <vector>
 
-class featureExtractor{
+template<class T>  
+class featureExtractor<std::vector<cv::DMatch>> {
 public:
 	std::pair<cv::Mat, std::vector<cv::KeyPoint>> ORB_detector(const cv::Mat& img);
 	cv::Mat ORB_compute(const cv::Mat& img, std::vector<cv::KeyPoint>& keypoints);
 	std::pair<cv::Mat, std::vector<cv::KeyPoint>> ORB_detectAndCompute(const cv::Mat& img);
 	std::vector<cv::KeyPoint> GFTT_detect(const cv::Mat& img);
 	std::vector<std::vector<cv::DMatch>> matching(const cv::Mat& descriptors1, const cv::Mat& descriptors2);
-	//std::vector<cv::KeyPoint> computeKeyPoints(const cv::Mat& img, const std::vector<cv::Point2f>& features);
-	//std::vector<cv::KeyPoint> mat2KeyPoints(const std::vector<cv::Point2f>& features);
-
-
 	cv::Mat drawKeyPoints(const cv::Mat& img, const std::vector<cv::KeyPoint>& keypoints) const;
 	cv::Mat drawKeyPoints(const cv::Mat& img, const std::vector<cv::Point2f>& keypoints) const;
 };
+
+class featureExtractor<std::vector<std::vector<cv::DMatch>>>{
+public:
+	std::pair<cv::Mat, std::vector<cv::DMatch>> ORB_detector(const cv::Mat& img);
+	cv::Mat ORB_compute(const cv::Mat& img,  
+
+}
